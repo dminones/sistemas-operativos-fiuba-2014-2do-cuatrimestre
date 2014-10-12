8c6095f5948b46db67ee34109833765630d86b79
kaoru <kaoru@kaoru-Inspiron-N4030>
2014-10-11 22:18:32 -0300

    borrado de lineas si se supera el largo del archivo

--- Logger.sh
+++ Logger.sh
@@ -4,14 +4,28 @@ start_paths(){
 	EXTENSION=".log"
 }
 
+count_lines(){
+	FILE=$1
+	LIMIT=10
+	LINES_TO_DELETE='1,5d'
+	LINES=`wc -l < $FILE`
+	if [ $LINES -ge $LIMIT ]; then
+		echo "tengo que borrar"
+		sed -i "$LINES_TO_DELETE" "$FILE"
+	fi
+	
+}
+
 log(){
 	start_paths
 	FILE=$1
+	FILE_PATH="$JOB_PATH$FILE$EXTENSION"
+	count_lines "$FILE_PATH"
+	
 	WHERE=$2
 	DATETIME=`date +'%Y/%m/%d %T'`
 	CODE=$3
 	DESCRIPTION=$4
-	FILE_PATH="$JOB_PATH$FILE$EXTENSION"
 	MESSAGE="$DATETIME - $USER - $WHERE - $CODE - $DESCRIPTION"
 	echo "$MESSAGE" >> "$FILE_PATH"
 }
