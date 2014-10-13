055260e020b5ca393b1582ee5c58edee4986138c
kaoru <kaoru@kaoru-Inspiron-N4030>
2014-10-13 12:45:30 -0300

    parsear csv en funciones aparte

--- FSoldes.sh
+++ FSoldes.sh
@@ -15,57 +15,57 @@
 start_paths(){
 	JOB_PATH="../../Documents/facultad/sistemasOperativos/tp/"
 	MAEDIR="maedir/"
+	MAEDIR_PATH="$JOB_PATH$MAEDIR"
 	ACEPDIR="acepdir/"
 	BANCOS="bancos.dat"
 	BANCOS_FILE="$JOB_PATH$MAEDIR$BANCOS"
 	LOG_FILE="logdir/FSoldes"
 	ACEPDIR_PATH="$JOB_PATH$ACEPDIR"
 	SALDOS_LIS_FILE="saldos.lis"
+	SALDOS_DIR="$MAEDIR_PATH""saldos/"
 }
 
 format_bank(){
 	NAME=$1
 	LINEA=`grep "^$NAME" "$BANCOS_FILE"`
-	
-#	CODIGO_ENTIDAD=`echo $LINEA | sed 's/^[^;]*;\([^;]*\).*/\1/'`
-#	UBIC_CBU=`echo $LINEA | sed 's/^[^;]*;[^;]*;[^;]*;\([^;]*\).*/\1/'`
-#	UBIC_SALDO=`echo $LINEA | sed 's/^[^;]*;[^;]*;[^;]*;[^;]*;\([^;]*\).*/\1/'`
-	FORMAT_CBU=`echo $LINEA | sed 's/^.*;//'`
-	
-	FIELD_BLOCK='\([^;]*;\)'
-	REPEAT=1
-	CODIGO_ENTIDAD=`echo $LINEA | sed "s/^$FIELD_BLOCK\{$REPEAT\}\([^;]*\).*/\2/"`
-	
-	REPEAT=3
-	UBIC_CBU=`echo $LINEA | sed "s/^$FIELD_BLOCK\{$REPEAT\}\([^;]*\).*/\2/"`
-	
-	REPEAT=4
-	UBIC_SALDO=`echo $LINEA | sed "s/^$FIELD_BLOCK\{$REPEAT\}\([^;]*\).*/\2/"`
-	
-#	echo $LINEA | sed 's/^\([^;]*;\)\{3\}\([^;]*\).*/\2/'
-#	echo "$UBIC_SALDO"	
+	FORMAT_CBU=`getLastCsvField "$LINEA"`	
+	CODIGO_ENTIDAD=`getCsvFieldNumber "$LINEA" 2`
+	UBIC_CBU=`getCsvFieldNumber "$LINEA" 4`
+	UBIC_SALDO=`getCsvFieldNumber "$LINEA" 5`
+}
+
+#$1:linea, $2:numero de campo
+getCsvFieldNumber(){
+	typeset FIELD_BLOCK='\([^;]*;\)'
+	typeset REPEAT=`expr $2 - 1`
+	echo $1 | sed "s/^$FIELD_BLOCK\{$REPEAT\}\([^;]*\).*/\2/"
+}
+
+#$1:linea
+getLastCsvField(){
+	echo $1 | sed 's/^.*;//'
 }
 
 processFile(){
 	FILE=$1	
 	log 'processFile' 'info' "Archivo a procesar:$FILE"
 	BANCO=`echo $FILE | sed 's/_.*$//'`
-	echo "banco: $BANCO"
 	FECHA=`echo $FILE | sed 's/^.*_//'`
 	format_bank "$BANCO"
-	echo "banco:$BANCO,entidad:$CODIGO_ENTIDAD ubic_cbu:$UBIC_CBU, ubic_saldo:$UBIC_SALDO, format_cbu:$FORMAT_CBU"
+	echo "banco:$BANCO,entidad:$CODIGO_ENTIDAD, ubic_cbu:$UBIC_CBU, ubic_saldo:$UBIC_SALDO, format_cbu:$FORMAT_CBU"
 	
-#	while read LINE;
-#	do
+	while read LINE;
+	do
 #		echo "$LINE";
-#	done < "$ACEPDIR_PATH$FILE"
+		processLine "$LINE";
+	done < "$ACEPDIR_PATH$FILE"
 }
 
 #por ahora solo paso como parametro LINE. Los otros datos los uso "globales". 
 #si despues aparece la necesidad lo cambio
 processLine(){
 	LINEA=$1
-	
+	echo "$FILE" >> "$SALDOS_DIR$SALDOS_LIS_FILE"
 }
 
 processAllFiles(){
